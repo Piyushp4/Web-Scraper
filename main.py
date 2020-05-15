091a037716d3d19787abb7c6a76fa28ce078616e
Piyushp4 <piyush@MacBook-Pro.local>
2020-05-15 15:59:02 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+URL = 'https://en.wikipedia.org/wiki/List_of_largest_manufacturing_companies_by_revenue'
+response = requests.get(URL)
+soup = BeautifulSoup(response.text,'html.parser')
+
+table = soup.find('table' , {'class' : 'wikitable sortable plainrowheads'}).tbody
+
+rows= table.find_all('tr')
+
+columns = [v.text.replace('\n','')for v in rows[0].find_all('th')]
+
+df = pd.DataFrame(columns=columns)
+
+for i in range(1,len(rows)):
+    tds = rows[i].find_all('td')
+
+    if len(tds) == 4:
+        values = [tds[0].text, tds[1].text, '' , tds[2].text, tds[3].text.replace('\n',''.replace('\xa0',''))]
+    else:
+         values = [td.text.replace('\n','').replace('\xa0','') for td in tds]
+
+    df = df.append(pd.Series(values, index=columns),ignore_index=True)
+
+    df.to_csv(r'/Users/piyush/Desktop/'+'//PWebScrape.csv',index=False)
